a47e915ba4986c6e90a60a8a2d945aac4e334331
b00t0x <agera5000@gmail.com>
2022-01-10 16:28:01 +0900

    refactoring

--- CpuTopologyRebuild/CpuTopologyRebuild.cpp
+++ CpuTopologyRebuild/CpuTopologyRebuild.cpp
@@ -1,4 +1,6 @@
 #define ACIDANTHERA_PRIVATE
+#define P_CORE_MAX_COUNT 32
+#define E_CORE_MAX_COUNT 64
 
 #include "CpuTopologyRebuild.h"
 #include <i386/cpu_topology.h>
@@ -12,12 +14,10 @@ bool ADDPR(debugEnabled) = true;
 uint32_t ADDPR(debugPrintDelay) = 0;
 
 bool smt_spoof = false;
-x86_lcpu_t *p0_cpus[8]; // P-Cores
-x86_lcpu_t *p1_cpus[8]; // P-Cores HT
-x86_lcpu_t *e0_cpus[8]; // E-Cores
-int p0_count = 0;
-int p1_count = 0;
-int e0_count = 0;
+x86_lcpu_t *p0_cpus[P_CORE_MAX_COUNT]; // P-Cores
+x86_lcpu_t *p1_cpus[P_CORE_MAX_COUNT]; // P-Cores HT
+x86_lcpu_t *e0_cpus[E_CORE_MAX_COUNT]; // E-Cores
+int p0_count, p1_count, e0_count;
 
 static void print_cache_info(x86_cpu_cache_t *cache) {
     x86_lcpu_t *cpu;
@@ -36,13 +36,12 @@ static void print_cache_info(x86_cpu_cache_t *cache) {
     char buf[5];
     char lcpus[256];
     lcpus[0] = '\0';
-    for (int i=0; i<32; ++i) {
-        cpu = cache->cpus[i];
-        if (cpu == NULL) {
-            break;
-        }
+    int i = 0;
+    cpu = cache->cpus[i];
+    while (cpu != nullptr) {
         snprintf(buf, 5, "%d,", cpu->pnum);
         strlcat(lcpus, buf, 256);
+        cpu = cache->cpus[++i];
     }
 
     DBGLOG("ctr", "  %s/type=%d/level=%d/%dKB/maxcpus=%d/nlcpus=%d/lcpus=%s",
@@ -59,7 +58,7 @@ static void print_cache_topology(void) {
     DBGLOG("ctr", "Cache info:");
     for (int i=2; i>=0; --i) { // LLC->L2->L1
         cpu = pkg->lcpus;
-        while (cpu != NULL) {
+        while (cpu != nullptr) {
             cache = cpu->caches[i];
             bool new_cache = true;
             for (int j=0; j<cache_count; ++j) {
@@ -84,10 +83,10 @@ static void print_cpu_topology(void) {
 
     DBGLOG("ctr", "CPU info:");
     core = pkg->cores;
-    while (core != NULL) {
+    while (core != nullptr) {
         DBGLOG("ctr", "  Core(p/l): %d/%d (lcpus: %d)", core->pcore_num, core->lcore_num, core->num_lcpus);
         cpu = core->lcpus;
-        while (cpu != NULL) {
+        while (cpu != nullptr) {
             DBGLOG("ctr", "    LCPU(n/p/l): %2d/%2d/%d", cpu->cpu_num, cpu->pnum, cpu->lnum);
             cpu = cpu->next_in_core;
         }
@@ -98,11 +97,13 @@ static void print_cpu_topology(void) {
 static void load_cpus(void) {
     x86_pkg_t  *pkg = x86_pkgs;
     x86_lcpu_t *cpu;
-    x86_lcpu_t *cpus_reverse[24];
+    x86_lcpu_t *cpus_reverse[P_CORE_MAX_COUNT * 2 + E_CORE_MAX_COUNT];
     int count = 0;
 
+    p0_count = p1_count = e0_count = 0;
+
     cpu = pkg->lcpus;
-    while (cpu != NULL) {
+    while (cpu != nullptr) {
         cpus_reverse[count++] = cpu;
         cpu = cpu->next_in_pkg;
     }
@@ -159,18 +160,14 @@ static void rebuild_cache_topology(void) {
         p1->caches[0] = l1;
         p1->caches[1] = l2;
 
-        l1->nlcpus = 2;
-        l2->nlcpus = 2;
-        l1->cpus[1] = p1;
-        l2->cpus[1] = p1;
+        l1->nlcpus = l2->nlcpus = 2;
+        l1->cpus[1] = l2->cpus[1] = p1;
     }
 }
 
 static void rebuild_cpu_topology(void) {
     // do nothing if E-Cores disabled
-    if (e0_count == 0) {
-        return;
-    }
+    if (e0_count == 0) return;
 
     x86_pkg_t  *pkg = x86_pkgs;
     x86_die_t  *die = pkg->dies;
@@ -179,13 +176,11 @@ static void rebuild_cpu_topology(void) {
 
     x86_core_t *p0_last = p0_cpus[p0_count - 1]->core;
     if (smt_spoof) {
-        pkg->cores = p0_last;
-        die->cores = p0_last;
+        pkg->cores = die->cores = p0_last;
         die->num_cores = p0_count;
     } else {
         core = e0_cpus[0]->core;
-        core->next_in_die = p0_last;
-        core->next_in_pkg = p0_last;
+        core->next_in_die = core->next_in_pkg = p0_last;
         die->num_cores = p0_count + e0_count;
     }
 
@@ -193,13 +188,11 @@ static void rebuild_cpu_topology(void) {
         cpu = p0_cpus[i];
         cpu->lnum = 0;
         core = cpu->core;
-        core->lcore_num = i;
-        core->pcore_num = i;
+        core->lcore_num = core->pcore_num = i;
         core->num_lcpus = 1;
         core->lcpus = cpu;
         if (i != 0) {
-            core->next_in_pkg = p0_cpus[i-1]->core;
-            core->next_in_die = p0_cpus[i-1]->core;
+            core->next_in_pkg = core->next_in_die = p0_cpus[i-1]->core;
         }
     }
     for (int i=0; i<p1_count; ++i) {
@@ -219,8 +212,7 @@ static void rebuild_cpu_topology(void) {
             cpu->next_in_core = core->lcpus;
             core->lcpus = cpu;
         } else {
-            cpu->core->lcore_num = p0_count + i;
-            cpu->core->pcore_num = p0_count + i;
+            cpu->core->lcore_num = cpu->core->pcore_num = p0_count + i;
         }
     }
 
